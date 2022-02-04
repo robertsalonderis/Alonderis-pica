5e2555fcc2088d5def788d612a3bf2ba6241765b
roberts <robertsalonderis@gmail.com>
2022-02-04 15:46:14 +0200

    Refaktorēts kods un ieviesta piegādes programmas daļa

--- src/GitPica.java
+++ src/GitPica.java
@@ -23,6 +23,24 @@ public class GitPica {
 	    System.out.println("Vârds un uzvârds: ");
 	    vards = scan.nextLine();
 
+	  //Piegâde
+	    String adrese;
+	    double atalums = 0;
+	    double piegadesMaksa = 0;
+	    System.out.println("Lûdzu ievadiet adresi:");
+	    adrese = scan.nextLine();
+	    System.out.println("Lûdzu ievadiet attâlumu kilometros no picçrijas(0 ja saòem uz vietas):");
+	    atalums = scan.nextDouble();
+	    if (atalums == 0) {
+	    	piegadesMaksa = 0;
+	        System.out.println("Nav jâmaksâ par piegâdi.");
+	    } else if (atalums > 1) {
+	    	piegadesMaksa = ((atalums * 0.5) + 2);
+	        System.out.println("Jûsu piegâdes maksa: $" + df.format(piegadesMaksa));
+	    } else if (atalums > 0) {
+	    	piegadesMaksa = 2.00;
+	        System.out.println("Jûsu piegâdes maksa: $" + df.format(piegadesMaksa));
+	    }
 	}
 
 }
