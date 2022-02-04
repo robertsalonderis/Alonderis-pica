a8d327bd6ec99604f737123be4d9b3046324cf9a
roberts <robertsalonderis@gmail.com>
2022-02-04 16:00:27 +0200

    Iespējams ievadīt picas mīklas veidu

--- src/GitPica.java
+++ src/GitPica.java
@@ -23,6 +23,57 @@ public class GitPica {
 	    System.out.println("Vârds un uzvârds: ");
 	    vards = scan.nextLine();
 
+	  //Piegâde
+	    String adrese;
+	    double atalums = 0;
+	    double piegadesMaksa = 0;
+	    System.out.println("Lûdzu ievadiet adresi:");
+	    adrese = scan.nextLine();
+	    System.out.println("Lûdzu ievadiet attâlumu kilometros no picçrijas(0 ja saòem uz vietas):");
+	    atalums = scan.nextDouble();
+	    if (atalums == 0) {
+	    	piegadesMaksa = 0;
+	        System.out.println("Nav jâmaksâ par piegâdi.");
+	    } else if (atalums > 1) {
+	    	piegadesMaksa = ((atalums * 0.5) + 2);
+	        System.out.println("Jûsu piegâdes maksa: $" + df.format(piegadesMaksa));
+	    } else if (atalums > 0) {
+	    	piegadesMaksa = 2.00;
+	        System.out.println("Jûsu piegâdes maksa: $" + df.format(piegadesMaksa));
+	    }
+	    
+	  //Pica(lielums)
+	    System.out.print("Cik lielu picu vçlaties(lielums cm)? (24, 30, 40, or 50) ");
+	    lielums = scan.nextInt();
+	    if (lielums == 20) {
+	        maksa = 7.10;
+	    } else if (lielums == 30) {
+	    	maksa = 8.35;
+	    } else if (lielums == 40) {
+	    	maksa = 12.00;
+	    } else if (lielums == 50) {
+	    	maksa = 17.10;
+	    } else if (lielums != 24 && lielums != 30 && lielums != 40 && lielums != 50) {
+	        System.out.println("Mums nav ðada lieluma picas, automâtiski izvçlçta 30cm.");
+	        maksa = 8.35;
+	    }
+	    scan.nextLine();
+	    
+	    //Pica(mîkla)
+	    System.out.print("Kâda veida mîklu vçlaties? (B)Biezâ, (C)Klasiskâ, vai (K)Kraukðíîgâ (Ievadi B, C, vai K,): ");
+	    miklasVeids = scan.nextLine().charAt(0);
+
+	    if (miklasVeids == 'B' || miklasVeids == 'b') {
+	    	mikla = "Biezâ";}
+	     else if (miklasVeids == 'C' || miklasVeids == 'c') {
+	    	mikla = "Klasiskâ";}
+	     else if (miklasVeids == 'K' || miklasVeids == 'k') {
+	    	mikla = "Kraukðíîgâ";}
+	     else if (miklasVeids != 'B' && miklasVeids != 'b' && miklasVeids != 'C' && miklasVeids != 'c' && miklasVeids != 'K' && miklasVeids != 'k') {
+	        System.out.println("Mums nav pieejama ðâda veida mîkla, jûsu mîkla bûs izvçlçta, kâ Klasiskâ. ");}
+	        mikla = "Klasiskâ";
+	    
+	    mikla = "Klasiskâ";
 	}
 
 }
