3fc3cd1ce91eb206858ed1c3ba355d50cdc3ab8a
roberts <robertsalonderis@gmail.com>
2022-02-04 15:07:18 +0200

    Izstrādāta picas piegādes programmas versija

--- src/GitPica.java
+++ src/GitPica.java
@@ -1,8 +1,27 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class GitPica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DecimalFormat df = new DecimalFormat("#.##");
+	    Scanner scan = new Scanner(System.in);
+	    
+	  //Mainîgie
+	    String vards;
+	    char miklasVeids;
+	    String mikla;
+	    int lielums;
+	    char picasVeids;
+	    String pica;
+	    double maksa = 0.0;
+	    final double pvnLikme = 0.08;
+	    double pvn;
+	    double summa;
+	    double kopSumma;
+	    
+	    System.out.println("Vârds un uzvârds: ");
+	    vards = scan.nextLine();
 
 	}
 
